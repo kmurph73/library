bae4fc091c285d8f3138e44d0a36881cc16ede03
Kyle Murphy <kmurph73@gmail.com>
2020-04-18 14:20:07 -0700

    dont export BrowserPDF417Reader

--- src/browser.ts
+++ src/browser.ts
@@ -3,7 +3,7 @@ export * from './browser/BrowserBarcodeReader';
 export * from './browser/BrowserCodeReader';
 export * from './browser/BrowserDatamatrixCodeReader';
 export * from './browser/BrowserMultiFormatReader';
-export * from './browser/BrowserPDF417Reader';
+// export * from './browser/BrowserPDF417Reader';
 export * from './browser/BrowserQRCodeReader';
 export * from './browser/BrowserQRCodeSvgWriter';
 export * from './browser/DecodeContinuouslyCallback';
